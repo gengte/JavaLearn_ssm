cccef24743bea9ca622fb7f2328e185eb86be8a9
gt <gengte@qq.com>
2019-10-31 16:01:31 +0800

    spring整合springMVC

--- src/main/java/com/g/controller/AccountController.java
+++ src/main/java/com/g/controller/AccountController.java
@@ -1,7 +1,12 @@
 package com.g.controller;
 
+import com.g.domain.Account;
+import com.g.services.IAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * 账户web层
@@ -10,8 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("account")
 public class AccountController {
 
+    @Autowired
+    private IAccountService accountService;
+
     @RequestMapping("findAll")
-    public String findAll(){
+    public String findAll() {
+        System.out.println("表现层：查询所有账户。。。");
+        List<Account> all = accountService.findAll();
         return "list";
     }
 }
