91d468680010a9be476b3ad8845e5576b081d022
gt <gengte@qq.com>
2019-10-31 17:27:59 +0800

    编写MyBatis框架。

--- src/main/java/com/g/dao/IAccountDao.java
+++ src/main/java/com/g/dao/IAccountDao.java
@@ -1,12 +1,16 @@
 package com.g.dao;
 
 import com.g.domain.Account;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface IAccountDao {
 
+    @Select("select * from account")
     public List<Account> findAll();
 
+    @Insert("insert into account (name,money) value (#{name},#{money})")
     public void saveAccount(Account account);
 }
---
+++ src/main/java/com/g/test/TestMyBatis.java
@@ -0,0 +1,36 @@
+package com.g.test;
+
+import com.g.dao.IAccountDao;
+import com.g.domain.Account;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class TestMyBatis {
+    @Test
+    public void run1() throws Exception {
+        //加载配置文件
+        InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //创建工厂
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+        //SqlSession对象
+        SqlSession sqlSession = factory.openSession();
+        //获取代理对象
+        IAccountDao mapper = sqlSession.getMapper(IAccountDao.class);
+//        Account newAccount = new Account();
+//        newAccount.setName("王五");
+//        newAccount.setMoney(220.0);
+//        mapper.saveAccount(newAccount);
+        List<Account> all = mapper.findAll();
+        for (Account account:all)
+            System.out.println(account);
+        //释放资源
+        sqlSession.close();
+        inputStream.close();
+    }
+}
